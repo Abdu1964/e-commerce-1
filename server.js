6cda7fef4ab3f1689519a7f33fabb0fe4c7b541c
unknown <email id>
2023-09-18 20:28:29 +0300

    route files wz express router

--- server.js
+++ server.js
@@ -3,22 +3,36 @@ const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI; // Assuming you have a separate file with the MongoDB URI
-//const connection_url= 'mongodb+srv://abdum14:Abtayd.rum@cluster0.ixyf4u9.mongodb.net/'
-// Connect to MongoDatabase
- mongoose
-   .connect(db)
-   .then(() => console.log('MongoDB connected'))
-   .catch(err => console.log(err));
 
-  //mongoose.connect(connection_url, {
-    //useNewUrlParser: true,
-    //useUnifiedTopology: true,
-     
-  //});
+// importing all API to the server.js
 
+const User = require('./routes/API/user');
+const Product = require('./routes/API/product');
+const Order = require('./routes/API/Order');
+const Cart = require('./routes/API/Cart');
+const Review = require('./routes/API/Review');
+const bodyParser = require('body-parser');
+
+// Connect to Mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+  
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// for route use
+app.use('/api/user', User);
+app.use('/api/cart', Cart);
+app.use('/api/order', Order);
+app.use('/api/product', Product);
+app.use('/api/review', Review);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
