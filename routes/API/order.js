887cc6875684d4a5efa1a4fc68b2195ad5fc9771
unknown <email id>
2023-09-24 14:57:27 +0300

    3.18 get current user's order

--- routes/API/order.js
+++ routes/API/order.js
@@ -1,29 +1,49 @@
 const express = require('express');
 const router= express.Router();
-
+const passport = require('passport');
+const Order = require('../../model/Order'); 
+const Product = require('../../model/product');
+const User = require('../../model/User');
 //@route GET api/users/test
 //@desc tests users route
 //@access public
 router.get('/test',(req , res) => res.json({msg: 'order  works'})
 );
+// @route   GET api/order
+// @desc    Get current user's order
+// @access  Private
+//to get the current users order..after they logged in
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
 
- 
-const Order = require('../../model/Order');
+  Order.findOne({user: req.user.id })
+    //.populate('user', ['name', 'avatar'])
+    .then(order => {
+      if (!order) {
+        errors.noorder = 'There is no order for this user';
+        console.log(errors)
+        return res.status(404).json(errors);
+      }
+      res.json(order);
+    })
+    .catch(err => res.status(404).json(err));
+}); 
+
 
 // GET /api/orders - Fetch all orders
-router.get('/', async (req, res) => {
+router.get('/orders', async (req, res) => {
   try {
-    const orders = await Order.find().populate('user', 'name');
+    const orders = await Order.find();
     res.json(orders);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// GET /api/orders/:id - Fetch a specific order
-router.get('/:id', async (req, res) => {
+ // GET /orders/:id - Fetch a specific product
+router.get('/orders/:id', async (req, res) => {
   try {
-    const order = await Order.findById(req.params.id).populate('user', 'name');
+    const order = await Order.findById(req.params.id);
     if (!order) {
       return res.status(404).json({ error: 'Order not found' });
     }
@@ -32,6 +52,7 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 
  // POST /api/orders
 router.post('/', async (req, res) => {
@@ -81,7 +102,7 @@ router.post('/', async (req, res) => {
 
 });
 // PUT /api/orders/:id - Update a specific order
-router.put('/:id', async (req, res) => {
+router.put('/orders/:id', async (req, res) => {
   try {
     const { user, products, totalAmount, shippingAddress, orderStatus } = req.body;
     const order = await Order.findByIdAndUpdate(req.params.id, {
@@ -112,7 +133,5 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-module.exports = router;
 
 module.exports = router;
